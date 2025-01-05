36ec2a06e5284edfaed2bbbdb74ed29ec59cb33f
surdeep-kannan <surdeepkannan2124@gmail.com>
2025-01-05 09:56:12 +0530

    added js

---
+++ darkmode.js
@@ -0,0 +1,25 @@
+let darkmode = localStorage.getItem('darkmode');
+const themeSwitch = document.getElementById('theme-switch');
+
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkmode', 'active');
+};
+
+const disableDarkmode = () => {
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkmode', 'inactive');
+};
+
+if (darkmode === "active") {
+    enableDarkmode();
+}
+
+themeSwitch.addEventListener("click", () => {
+    darkmode = localStorage.getItem('darkmode');
+    if (darkmode === "active") {
+        disableDarkmode();
+    } else {
+        enableDarkmode();
+    }
+});
